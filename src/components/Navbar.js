d762b58b51a06e91653dce7de148832dd1753377
Agustin Gabriel Perez <perezagustin95@hotmail.com>
2020-09-13 11:47:59 -0300

    Landing completa con animaciones y scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import "./cssComponents/navbar.css"
-
+import brand from '../images/brand.png'
+import { Link, animateScroll, scrollSpy } from 'react-scroll'
+import { Container } from 'react-bootstrap'
 
 const Navbar = () => {
 
+  const [backgroundNav, setBackgroundNav] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setBackgroundNav(true)
+    } else {
+      setBackgroundNav(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground)
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"></a>
+    <nav id="mainNav" className="navbar bg-color navbar-expand-lg navbar-light fixed-top" style={backgroundNav ? { backgroundColor: "white" } : { backgroundColor: "transparent" }}>
+      <Container>
+        <div className="navbar-brand" onClick={() => { animateScroll.scrollToTop() }}>
+          <img src={backgroundNav ? brand : ""} alt="" />
+        </div>
         <button
-          class="navbar-toggler navbar-toggler-right"
+          className="navbar-toggler navbar-toggler-right"
           type="button"
           data-toggle="collapse"
           data-target="#navbarResponsive"
@@ -19,24 +34,23 @@ const Navbar = () => {
           aria-label="Toggle navigation"
         >
           Menu
-        <i class="fas fa-bars"></i>
+        <i className="fas fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">Nosotros</a>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className={backgroundNav ? "nav-link default" : "nav-link"} activeClass="active" spy={true} to="about" smooth={true} duration={1000} >Nosotros</Link>
             </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#projects"
-              >Proyectos</a
-              >
+            <li className="nav-item">
+              <Link className={backgroundNav ? "nav-link default" : "nav-link"} activeClass="active" spy={true} to="projects" smooth={true} duration={1000}
+              >Proyectos</Link>
             </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#signup">Contacto</a>
+            <li className="nav-item">
+              <Link className={backgroundNav ? "nav-link default" : "nav-link"} activeClass="active" spy={true} to="signup" smooth={true} duration={1000}>Contacto</Link>
             </li>
           </ul>
         </div>
-      </div>
+      </Container>
     </nav>
 
   )
