4a79010d21ff2f93bab10d35acec7e312413ce9f
Agustin Gabriel Perez <perezagustin95@hotmail.com>
2020-11-29 20:31:01 -0300

    Diseño responsivo de ts

--- src/components/routes/TS_InfoCompleta.js
+++ src/components/routes/TS_InfoCompleta.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import firebase from '../../services/firebase';
-import { Container, Spinner, Button } from 'react-bootstrap';
+import { Container, Spinner, Button, Col, Row } from 'react-bootstrap';
 import '../cssComponents/routes.css'
 import { Link } from 'react-router-dom';
 
@@ -40,25 +40,51 @@ const InfoCompleta = (props) => {
         <div id="section" >
           <div className="espaciado"></div>
           <Container className="titulos ">
-            <h1 className="text-center">
-              <b>{producto.title}</b>
-            </h1>
+            <Container>
+              <Row>
+                <Col xs={12} md={12}>
+                  <h1 className="text-center">
+                    <b>{producto.title}</b>
+                  </h1>
+                </Col>
+              </Row>
+            </Container>
             <br />
             <div>
-              <div className="d-flex justify-content-left">
-                <img className="m-3 titulos" src={producto.img} alt={producto.shortDescription} style={{ height: "20rem" }} />
-                <h3 className="m-3">
-                  {producto.description}
-                </h3>
-              </div>
-              <div className="d-flex justify-content-left">
-                <h3 className="m-3"><b>Link:</b> <a style={{ color: 'rgb(0,0,0)' }} href={producto.linkInscripcion} target="_blanck">GoogleForm</a></h3>
-                <h3 className="m-3"><b>Fecha de inicio:</b> {producto.fecha}</h3>
-                <h3 className="m-3"><b>Dificultad:</b> {producto.dificultad}</h3>
-              </div>
+              <Container className="d-flex justify-content-left">
+                <Row>
+                  <Col xs={12} md={4}>
+                    <img
+                      className=" titulos"
+                      src={producto.img}
+                      alt={producto.shortDescription}
+                      style={{ height: "20rem" }}
+                    />
+                  </Col>
+                  <Col xs={12} md={8}>
+                    <h3 className="m-3">
+                      {producto.description}
+                    </h3>
+                  </Col>
+                </Row>
+              </Container>
+              <br />
+              <Container className="d-flex justify-content-left">
+                <Row>
+                  <Col xs={12} md={4}>
+                    <h3 className=""><b>Link:</b> <a style={{ color: 'rgb(0,0,0)' }} href={producto.linkInscripcion} target="_blanck">GoogleForm</a></h3>
+                  </Col>
+                  <Col xs={12} md={4}>
+                    <h3 className=""><b>Fecha de inicio:</b> {producto.fecha}</h3>
+                  </Col>
+                  <Col xs={12} md={4}>
+                    <h3 className=""><b>Dificultad:</b> {producto.dificultad}</h3>
+                  </Col>
+                </Row>
+              </Container>
             </div>
-
             <div className="espaciado"></div>
+
             <Button variant="primary"><Link style={{ color: 'rgb(255,255,255)' }} to="/talleres">Volver</Link></Button>
           </Container>
         </div>
